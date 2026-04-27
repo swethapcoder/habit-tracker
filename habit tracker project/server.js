5e4586d0fa9537380ba8b01701f787a71c25fb15
swetha <167347813+swethapcoder@users.noreply.github.com>
2026-04-27 12:03:26 +0530

    Update server.js

--- habit tracker project/server.js
+++ habit tracker project/server.js
@@ -22,6 +22,13 @@ app.use(cors());
 app.use(express.json());
 
 // ----------------- MONGODB CONNECTION -----------------
+const MONGO_URI = process.env.MONGO_URI;
+
+if (!MONGO_URI) {
+  console.error("❌ MONGO_URI is not defined in environment variables");
+  process.exit(1);
+}
+
 mongoose.connect(MONGO_URI)
   .then(() => console.log('💾 MongoDB connected (nosql)'))
   .catch(err => console.error('❌ MongoDB connection error:', err));
